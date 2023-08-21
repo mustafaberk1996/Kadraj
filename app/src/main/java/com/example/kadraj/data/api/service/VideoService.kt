095e7c8cd14f88b963277eed8016b05af7354f7a
nejat <nejatbaturay@gmail.com>
2023-08-21 13:49:47 +0300

    Video service and model implementation

---
+++ app/src/main/java/com/example/kadraj/data/api/service/VideoService.kt
@@ -0,0 +1,18 @@
+package com.example.kadraj.data.api.service
+
+import com.example.kadraj.Constants
+import com.example.kadraj.data.api.model.ResponseVideoList
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface VideoService {
+
+    @Headers(
+        "Authorization:${Constants.API_KEY}"
+    )
+
+    @GET("videos/search")
+    suspend fun getAll(@Query("query") queryText:String): ResponseVideoList
+
+}
