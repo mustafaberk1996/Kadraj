3aacceeef9122d90d41e0fd4a8e36dfc9b0e702e
metinyildiran <metin.1464@gmail.com>
2023-08-21 11:36:45 +0300

    Added app database with Hilt.

---
+++ app/src/main/java/com/example/kadraj/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.kadraj
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.kadraj.Constants.DATABASE_NAME
+import com.example.kadraj.data.model.User
+
+@Database(
+    entities = [User::class], version = 1, exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    // TODO add all DAOs
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        operator fun invoke(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME).build()
+        }
+    }
+}
