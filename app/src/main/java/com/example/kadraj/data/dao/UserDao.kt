41d0cb904f848cbe04e2ef555d19412d993b8c75
Oğuz <toruntay81@gmail.com>
2023-08-21 11:44:22 +0300

    User singin functions added.

--- app/src/main/java/com/example/kadraj/data/dao/UserDao.kt
+++ app/src/main/java/com/example/kadraj/data/dao/UserDao.kt
@@ -2,7 +2,10 @@ package com.example.kadraj.data.dao
 
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.kadraj.data.entity.User
 
 @Dao
@@ -10,6 +13,19 @@ interface UserDao {
 
     @Insert
     suspend fun insert(user: User)
+
+    @Query("SELECT * FROM User")
+    suspend fun getAll():List<User>
+
+
+    @Query("SELECT * FROM User WHERE id = :id")
+    suspend fun getUserById(id: Int): User
+
+    @Update
+    suspend fun update(user:User)
+
+    @Delete
+    suspend fun delete(user:User)
 
 
 }
