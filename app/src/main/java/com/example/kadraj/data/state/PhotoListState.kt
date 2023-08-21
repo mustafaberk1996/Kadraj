dbaa7f4521bde5e75581d0f5d00423f39822dce4
Salih Özsoy <salihhozsoy@gmail.com>
2023-08-21 12:20:14 +0300

    PhotoListState and PhotoViewModel added

---
+++ app/src/main/java/com/example/kadraj/data/state/PhotoListState.kt
@@ -0,0 +1,11 @@
+package com.example.kadraj.data.state
+
+import com.example.kadraj.data.api.model.Photo
+
+sealed class PhotoListState{
+    object Idle:PhotoListState()
+    object Loading:PhotoListState()
+    object Empty:PhotoListState()
+    class Result(val photos:List<Photo>):PhotoListState()
+    class Error(val throwable: Throwable):PhotoListState()
+}
