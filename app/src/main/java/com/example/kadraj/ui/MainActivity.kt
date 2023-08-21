fc96ea26afd6e508ecacfe84c5490ca047e11f8c
Büşra Üstünel <69143203+busraustunel@users.noreply.github.com>
2023-08-21 11:24:55 -0700

    add ui package

--- app/src/main/java/com/example/kadraj/ui/MainActivity.kt
+++ app/src/main/java/com/example/kadraj/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kadraj
+package com.example.kadraj.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kadraj.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
