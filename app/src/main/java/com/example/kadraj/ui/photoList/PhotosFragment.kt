38fff0d4ed3cfd88339d48d2e9527db3bb2e74c6
Oğuz <toruntay81@gmail.com>
2023-08-21 11:38:42 +0300

    Photo RecyclerView Added.

--- app/src/main/java/com/example/kadraj/ui/photoList/PhotosFragment.kt
+++ app/src/main/java/com/example/kadraj/ui/photoList/PhotosFragment.kt
@@ -1,10 +1,9 @@
-package com.example.kadraj
+package com.example.kadraj.ui.photoList
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import com.example.kadraj.R
 
 
 class PhotosFragment : Fragment(R.layout.fragment_photos) {
---
+++ app/src/main/java/com/example/kadraj/ui/photoList/PhotosRecyclerAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.kadraj.ui.photoList
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.kadraj.data.api.model.Photo
+import com.example.kadraj.databinding.PhotoListItemBinding
+
+class PhotoAdapter(private val context: Context, private val photos:List<Photo>, val onClick:(photo:Photo)->Unit):
+    RecyclerView.Adapter<PhotoAdapter.MyViewHolder>() {
+
+
+    class MyViewHolder(binding: PhotoListItemBinding): RecyclerView.ViewHolder(binding.root){
+
+        val ivPhoto=binding.ivPhoto
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            PhotoListItemBinding.inflate(LayoutInflater.from(context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return photos.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val photo=photos[position]
+           holder.ivPhoto.load(photo.src.medium)
+
+            holder.itemView.setOnClickListener {
+                //    onClick(this)
+            }
+        }
+    }
