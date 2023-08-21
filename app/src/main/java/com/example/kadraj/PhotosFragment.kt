c809d2edb7ab64864fe2c508fe08dfb4b831b0ce
gözde <aykentgozde96@gmail.com>
2023-08-21 10:50:47 +0300

    photo fragment added

---
+++ app/src/main/java/com/example/kadraj/PhotosFragment.kt
@@ -0,0 +1,15 @@
+package com.example.kadraj
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class PhotosFragment : Fragment(R.layout.fragment_photos) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
