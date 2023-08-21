7a5f8a0240033177c9c34658032b440ed539d413
Büşra Üstünel <69143203+busraustunel@users.noreply.github.com>
2023-08-21 12:06:47 -0700

    change ui packages

--- app/src/main/java/com/example/kadraj/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/kadraj/ui/main/MainActivity.kt
@@ -2,27 +2,13 @@ package com.example.kadraj.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 import com.example.kadraj.R
-import com.example.kadraj.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-    class MainActivity : AppCompatActivity() {
-        private lateinit var binding:ActivityMainBinding
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-
-            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-            val navController = navHostFragment.navController
-
-            val appBarConfiguration = AppBarConfiguration(navController.graph)
-
-            binding.toolbar.setupWithNavController(navController, appBarConfiguration)
-
-        }
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
+}
