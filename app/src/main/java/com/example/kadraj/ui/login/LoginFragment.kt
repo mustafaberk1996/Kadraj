857199928f28e5293bc8d5510d40ac1d9271730f
Büşra Üstünel <69143203+busraustunel@users.noreply.github.com>
2023-08-21 12:06:47 -0700

    change ui packages

--- app/src/main/java/com/example/kadraj/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/kadraj/ui/login/LoginFragment.kt
@@ -1,15 +1,13 @@
-package com.example.kadraj.ui
+package com.example.kadraj.ui.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import com.example.kadraj.R
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class PhotosFragment : Fragment(R.layout.fragment_photos) {
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
-
 }
--- app/src/main/java/com/example/kadraj/ui/photos/PhotosFragment.kt
+++ app/src/main/java/com/example/kadraj/ui/photos/PhotosFragment.kt
@@ -1,14 +1,15 @@
-package com.example.kadraj
+package com.example.kadraj.ui.photos
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import com.example.kadraj.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
-
+@AndroidEntryPoint
+class PhotosFragment : Fragment(R.layout.fragment_photos) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
 }
