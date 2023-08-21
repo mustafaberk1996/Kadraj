f807bc39b8353e75e213e48c540966460a7afee5
medine <medine_urun@hotmail.com>
2023-08-21 11:03:39 +0300

    get photo from api

---
+++ app/src/main/java/com/example/kadraj/data/api/model/Response.kt
@@ -0,0 +1,9 @@
+package com.example.kadraj.data.api.model
+
+data class Response(
+    val next_page: String,
+    val page: Int,
+    val per_page: Int,
+    val photos: List<Photo>,
+    val total_results: Int
+)
