f25a765b486c80262fca2c90a6256f0d45488def
oguzhanyigit <oguzhanveyigit@gmail.com>
2023-08-22 11:25:21 +0300

    EditText'e girilen değerlere göre butonun tıklanabilirliği ve renkleri değişiyor

--- app/src/main/java/com/example/kadraj/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/kadraj/fragment/LoginFragment.kt
@@ -1,9 +1,13 @@
 package com.example.kadraj.fragment
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -21,8 +25,22 @@ import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    @SuppressLint("ResourceAsColor")
+    fun checkMailAndPassword(email:String, password:String) {
+        if (email.isNullOrEmpty() || password.isNullOrEmpty()) {
+            binding.btnLogin.isClickable = false
+            val passiveColor = ContextCompat.getColor(requireContext(), R.color.loginpassive)
+            binding.btnLogin.setBackgroundColor(passiveColor)
+        } else {
+            binding.btnLogin.isClickable = true
+            val activeColor = ContextCompat.getColor(requireContext(), R.color.loginactive)
+            binding.btnLogin.setBackgroundColor(activeColor)
+        }
+    }
+
     private lateinit var binding:FragmentLoginBinding
     private val viewModel:LoginViewModel by viewModels()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,6 +52,34 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             viewModel.login(AppDatabase.invoke(requireContext()) ,binding.etUser.text.toString(), binding.etPassword.text.toString())
         }
 
+
+        binding.etUser.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                checkMailAndPassword(binding.etUser.text.toString(), binding.etPassword.text.toString())
+            }
+
+        })
+
+        binding.etPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                checkMailAndPassword(binding.etUser.text.toString(), binding.etPassword.text.toString())
+            }
+
+        })
     }
 
     private fun observeLogin() {
@@ -56,6 +102,11 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        binding.btnLogin.isClickable = false
+        super.onResume()
     }
 }
 
